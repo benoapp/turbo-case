79373dc6bae133a830a836e64ca01a49dfe5d87a
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2023-12-29 00:36:39 +0400

    refactor: exported code that creates the parser to functions

--- src/cliapp/main.py
+++ src/cliapp/main.py
@@ -3,39 +3,13 @@ import utility
 from TestManagementSystems.Factory import Factory
 from rich import print as pprint
 
-
-def parse_args(parser: argparse.ArgumentParser):
-    args = parser.parse_args()
-
-    if not any(vars(args).values()):
-        utility.print_banner()
-        parser.print_help()
-
-    if args.selected_command == "create":
-        created_files_n = 0
-        for file_path in args.files:
-            try:
-                test_management_system = Factory.get_test_management_system(args.system)
-                test_management_system.create_test_case(file_path, args.api_key)
-                created_files_n += 1
-            except Exception as e:
-                pprint(
-                    f"[red][ERR] Failed to create test case from file: [yellow]`{file_path}`[/yellow]. Reason:\n{e}\n"
-                )
-
-        test_case_plural = "s" if len(args.files) > 1 else ""
-        pprint(
-            f"[green]Created [cyan]{created_files_n}/{len(args.files)}[/cyan] test case{test_case_plural}."
-        )
+HELP_MESSAGE = "Show help"
 
 
-def main():
-    HELP_MESSAGE = "Show help"
-
-    # main- and sub-parsers
+def create_main_and_sub_parsers():
     parser = argparse.ArgumentParser(
         prog="turbocase",
-        description="TurboCase is a helper CLI App that enables manual-test-as-code",
+        description="Turbo-Case is a helper CLI App that enables manual-test-as-code",
         add_help=False,
         usage="%(prog)s [command] [options]",
         formatter_class=utility.CustomHelpFormatter,
@@ -48,7 +22,10 @@ def main():
         help="Use '%(prog)s <command> --help' for more information",
     )
 
-    # global arguments
+    return parser, subparsers
+
+
+def add_global_options(parser: argparse.ArgumentParser):
     parser.add_argument(
         "-h",
         "--help",
@@ -63,7 +40,8 @@ def main():
         help="Show program's version",
     )
 
-    # subparser for 'create' command
+
+def add_create_command(subparsers: argparse._SubParsersAction):
     create_parser = subparsers.add_parser(
         "create",
         help="Create test cases from YAML files",
@@ -100,6 +78,42 @@ def main():
         action="help",
         help=HELP_MESSAGE,
     )
+
+
+def handle_create_command(args):
+    created_files_n = 0
+    for file_path in args.files:
+        try:
+            test_management_system = Factory.get_test_management_system(args.system)
+            test_management_system.create_test_case(file_path, args.api_key)
+            created_files_n += 1
+        except Exception as e:
+            pprint(
+                f"[red][ERR] Failed to create test case from file: [yellow]`{file_path}`[/yellow]. Reason:\n{e}\n"
+            )
+
+    test_case_plural = "s" if len(args.files) > 1 else ""
+    pprint(
+        f"[green]Created [cyan]{created_files_n}/{len(args.files)}[/cyan] test case{test_case_plural}."
+    )
+
+def parse_args(parser: argparse.ArgumentParser):
+    args = parser.parse_args()
+
+    if args.selected_command is None:
+        utility.print_banner()
+        parser.print_help()
+
+    if args.selected_command == "create":
+        handle_create_command(args)
+
+
+def main():
+    parser, subparsers = create_main_and_sub_parsers()
+
+    add_global_options(parser)
+
+    add_create_command(subparsers)
 
     parse_args(parser)
 
