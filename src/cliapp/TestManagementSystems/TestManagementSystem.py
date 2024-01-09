caacf015a15cb9b2971be98f51950d0d33439cab
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2024-01-09 19:23:16 +0400

    feat: `insert_test_case` returns `ID` instead of `None`

--- src/cliapp/TestManagementSystems/TestManagementSystem.py
+++ src/cliapp/TestManagementSystems/TestManagementSystem.py
@@ -6,12 +6,15 @@ from overrides import EnforceOverrides
 class TestManagementSystem(ABC, EnforceOverrides):
     @staticmethod
     @abstractmethod
-    def create_test_case(file_path: str, api_key: str):
+    def create_test_case(file_path: str, api_key: str) -> int:
         """Creates a test case from a YAML file using the passed API key
 
         Args:
             file_path (str): path to the YAML file containing the test case
             api_key (str): API key to use for creating the test case
+
+        Returns:
+            int: The ID of the created test case
         """
         pass
 
